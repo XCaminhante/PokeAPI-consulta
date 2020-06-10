afcf0940dbeacc1c4d0231f83b30a898158b0a19
XCaminhante <caminhante@i2pmail.org>
2020-06-10 20:21:12 -0300

    Decidi as interfaces, falta implementar

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const axios = require('axios')
+const rs = require('readline-sync')
 function FontePokemonAxios (_nomePokemon) {
   async function pokemon () {
-    var r = await axios.get(`https://pokeapi.co/api/v2/pokemon/${_nomePokemon}`)
-    return new Pokemon(r.data)
+    try {
+      var r = await axios.get(`https://pokeapi.co/api/v2/pokemon/${_nomePokemon}`)
+      return new Pokemon(r.data)
+    } catch (e) {
+      return false
+    }
   }
   return {pokemon}
+}
+function ArmazemArquivosJSON (_caminho) {
+  function contem (nome) {
+    return false
+  }
+  function recuperar (nome) {
+    return false
+  }
+  function salvar (dadosPokemon) {
+    return false
+  }
+  return {contem,recuperar,salvar}
 }
 function Pokemon (_dados) {
   function nome () { return _dados.name }
@@ -17,7 +34,10 @@ Tipos:
 Habilidades:
   ${habilidades()}`
   }
-  return {nome,tipos,habilidades,toString}
+  function salvarCom (armazem) {
+    return armazem.salvar(_dados)
+  }
+  return {nome,tipos,habilidades,toString,salvarCom}
 }
 function TiposPokemon (_dados) {
   function nomes () {
@@ -41,7 +61,14 @@ function RelacoesDanosPokemon (_dados) {
   
 }
 async function main () {
-  var p = await new FontePokemonAxios('ditto').pokemon()
+  var n = rs.question('Nome de Pokemón: ').trim()
+  var a = new ArmazemArquivosJSON('./.pokemons')
+  if (a.contem(n)) {
+    var p = a.recuperar(n)
+  } else {
+    var p = await new FontePokemonAxios(n).pokemon()
+  }
+  if (!p) { console.log('Não consegui carregar o pokemón, tente de novo'); return }
   console.log( p.toString() )
 }
 main()
