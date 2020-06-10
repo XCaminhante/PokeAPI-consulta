c3c9522533b1ea1c2a9983c132db1e76bbd3af0d
XCaminhante <caminhante@i2pmail.org>
2020-06-10 19:58:46 -0300

    Apresentação ok, falta pedir no console e salvar pokemóns

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
 const axios = require('axios')
 function FontePokemonAxios (_nomePokemon) {
-  function pokemon () {}
+  async function pokemon () {
+    var r = await axios.get(`https://pokeapi.co/api/v2/pokemon/${_nomePokemon}`)
+    return new Pokemon(r.data)
+  }
+  return {pokemon}
 }
 function Pokemon (_dados) {
   function nome () { return _dados.name }
   function tipos () { return new TiposPokemon(_dados.types) }
   function habilidades () { return new HabilidadesPokemon(_dados.abilities) }
-  return {nome,tipos,habilidades}
+  function toString () {
+    return `Pokemon: ${nome()}
+Tipos:
+  ${tipos()}
+Habilidades:
+  ${habilidades()}`
+  }
+  return {nome,tipos,habilidades,toString}
 }
 function TiposPokemon (_dados) {
   function nomes () {
     return _dados.map( (t) => t.type.name )
   }
-  return {nomes}
+  function toString () {
+    return nomes().join(', ')
+  }
+  return {nomes,toString}
 }
 function HabilidadesPokemon (_dados) {
   function nomes () {
     return _dados.map( (t) => t.ability.name )
   }
-  return {nomes}
+  function toString () {
+    return nomes().join(', ')
+  }
+  return {nomes,toString}
 }
 function RelacoesDanosPokemon (_dados) {
   
 }
 async function main () {
-  var r = await axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
-  var p = new Pokemon(r.data)
-  var ts = p.tipos()
-  var hs = p.habilidades()
-  console.log( hs.nomes() )
+  var p = await new FontePokemonAxios('ditto').pokemon()
+  console.log( p.toString() )
 }
 main()
